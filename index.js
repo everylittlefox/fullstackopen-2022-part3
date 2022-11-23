0c074dfa4e4d83219e549e4db63905b0d9333697
Favour Anefu <favouranefu@gmail.com>
2022-11-23 21:55:56 +0100

    completed first batch of exercises for part3-a

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  return res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find((p) => p.id === id);
+  return person
+    ? res.json(person)
+    : res.status(404).json({ error: "person not found." });
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+
+  if (!person.name)
+    return res.status(400).json({ error: "name cannot be empty." });
+  if (!person.number)
+    return res.status(400).json({ error: "number cannot be empty." });
+
+  const personWithNameExists = persons.some(
+    (p) => p.name.toLowerCase() === person.name.toLowerCase()
+  );
+  if (personWithNameExists)
+    return res.status(400).json({ error: "name must be unique" });
+
+  const newPerson = { ...person, id: Math.floor(Math.random() * 1000000) };
+  persons.push(newPerson);
+
+  return res.status(201).json(newPerson);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (!person) return res.status(404).json({ error: "person not found." });
+
+  persons = persons.filter((p) => p.id !== id);
+
+  return res.status(204).end();
+});
+
+app.get("/info", (req, res) => {
+  const html = `<div>
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date().toUTCString()}</p>
+    </div>`;
+  return res.send(html);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
